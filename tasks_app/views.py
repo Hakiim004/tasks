4ebc806f72a7feae83ebeefd27bd046fd68df856
OUEDRAOGO Wend-Nonga  Abdoul Hakiim <abdoulhakiim25wgithub@gmail.com>
2025-06-30 20:42:41 +0000

    Update views.py

--- tasks_app/views.py
+++ tasks_app/views.py
@@ -3,7 +3,13 @@ from django.shortcuts import redirect, render , get_object_or_404
 from tasks_app.models import Task
 from .forms import task_appForm
 # Create your views here.
+----------------------------------------------------------------------------------------------------------------------------------------------------
+Si l'utilisateur soumet un formulaire (méthode POST), les données sont récupérées.
 
+form.is_valid() : vérifie que les données sont valides.
+
+form.save() : enregistre la nouvelle tâche dans la base de données.
+----------------------------------------------------------------------------------------------------------------------------------------------------
 def task_view(request):
     if request.method == 'POST':
         form = task_appForm(request.POST)
@@ -16,14 +22,39 @@ def task_view(request):
                'tasks':tasks ,
     }
     return render(request, 'home.html' , context)
+------------------------------------------------------------------------------------------------------------------------------------------------------
+Cette fonction gère la modification d'une tâche existante.
+
+request : la requête HTTP.
+
+pk : la clé primaire de la tâche à modifier (chaque tâche a un identifiant unique dans la base de données).
+
+Essaie de récupérer la tâche avec l’ID pk.
+
+Si elle n’existe pas, retourne automatiquement une erreur 404 Not Found.
+
 
 
 def update_task(request , pk):
     tasks = get_object_or_404(Task  , pk = pk  )
     form = task_appForm(request.POST or None , instance= tasks)
+
+    Si c’est une requête POST : on prend les données envoyées.
+
+    Si ce n’est pas une requête POST (donc GET), on envoie juste None, donc le formulaire affichera les données existantes de la tâche.
+
+    instance=tasks signifie que le formulaire est lié à cette tâche précise.
+
+
     if form.is_valid():
         form.save()
-        redirect('home')
+Si les données envoyées dans le formulaire sont valides :
+
+    on les enregistre dans la base de données.
+
+    on redirige vers la page home (⚠️ problème ici, voir remarque ci-dessous).
+
+    return     redirect('home')
     return render(request , 'update.html' ,{'form':form})
 
 def delete_task(request , pk):
